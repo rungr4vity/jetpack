b519a9e025fc1191621da8379c47226d4af965b3
fcomart <francisco.perez@isita.com.mx>
2024-03-14 16:47:12 -0600

    Biometric

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # jetpack
-Android con Jetpack Compose
+Android con biometrics
+
+
+- add the permissions
+
+    uses-permission android:name="android.permission.INTERNET" 
+    uses-permission android:name="android.permission.POST_NOTIFICATIONS" 
+
+
+- add the dependency biometric in the gradle/app and the appcombat compability
+  // biometric
+  implem..("androidx.biometric:biometric:1.1.0")
+  impleme..("androidx.appcompat:appcompat:1.6.1")
--- app/src/main/java/com/example/jetpack/MainActivity.kt
+++ app/src/main/java/com/example/jetpack/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.jetpack
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.biometric.BiometricPrompt
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,9 +12,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
 import com.example.jetpack.ui.theme.JetpackTheme
 
-class MainActivity : ComponentActivity() {
+//ComponentActivity
+
+//old
+//AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -27,6 +35,50 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+
+
+
+   private var canAuthenticate = false
+   private lateinit var promptInfo: BiometricPrompt.PromptInfo
+   fun setupAuth() {
+       if(androidx.biometric.BiometricManager.from(this).canAuthenticate(
+               androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG or
+                       androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL) == androidx.biometric.BiometricManager.BIOMETRIC_SUCCESS) {
+
+               canAuthenticate = true
+               promptInfo = BiometricPrompt.PromptInfo.Builder()
+                   .setTitle("Auth biométrica")
+                   .setSubtitle("Autenticate utilizando el sensor biométrico")
+                   .setAllowedAuthenticators(androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG or
+                   androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL)
+                   .build()
+
+       }
+   }
+
+
+   fun authenticate(auth: (auth: Boolean) -> Unit) {
+       if (canAuthenticate) {
+           BiometricPrompt(this, ContextCompat.getMainExecutor(this),
+               object : BiometricPrompt.AuthenticationCallback() {
+
+                   override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                       super.onAuthenticationSucceeded(result)
+
+                       auth(true)
+                   }
+           }).authenticate(promptInfo)
+       } else {
+           auth(true)
+       }
+   }
+
+
+
+
+
+
 }
 
 @Composable
